7aad9f36d0782e2b9f175d8a6663476295c873ce
menna <menaemad550@outlook.com>
2017-03-27 21:21:38 +0200

    first

---
+++ BOOK_READER/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BOOK_READER
+{
+	[Activity(Label = "BOOK_READER", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+
+			Button button1 = FindViewById<Button>(Resource.Id.btnSignup);
+			Button button2 = FindViewById<Button>(Resource.Id.btnLogin);
+
+			button1.Click += delegate { StartActivity(typeof(signupActivity)); };
+			button2.Click += delegate { StartActivity(typeof(homeActivity)); };
+
+		}
+	}
+}
+
+
---
+++ BOOK_READER/Resources/homeActivity.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BOOK_READER
+{
+	[Activity(Label = "homeActivity")]
+	public class homeActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.home);
+			// Create your application here
+		}
+	}
+}
+
